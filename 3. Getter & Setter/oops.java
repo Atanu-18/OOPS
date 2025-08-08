99840ec84848c2a0872d1a643ba6f526c3dec8a6
ATANU MANDAL <atanu2697@gmail.com>
2025-08-08 21:46:50 +0530

    Add files via upload

---
+++ 3. Getter & Setter/oops.java
@@ -0,0 +1,48 @@
+public class oops {
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); // created a Pen object called p1
+
+        // Set color
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+
+        // Set tip size
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+
+
+        p1.setColor("Red");
+        System.out.println(p1.getColor());
+
+
+        p1.setTip(2);
+        System.out.println(p1.getTip());
+        
+
+
+    }
+}
+
+class Pen {
+    //properties
+    private String color;
+    private int tip;
+
+    //get method
+    String getColor() {
+        return this.color;
+    }
+    int getTip() {
+        return this.tip;
+    }
+
+    //set method
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+}
+
